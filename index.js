1cd51f91491a63ac55af369832fec793d63d0abf
strarsis <strarsis@gmail.com>
2015-09-14 01:18:04 +0200

    Adjust jhint config. Make jhint pass.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+'use strict';
+
 var escapeRegexp  = require('escape-string-regexp');
 
 var strReplaceAll = function(needle, replace, haystack) {
-  if(needle.length == 0 || haystack.length == 0) return haystack;
+  if(needle.length === 0 || haystack.length === 0) {
+    return haystack;
+  }
   return haystack.replace(
     new RegExp(escapeRegexp(needle), 'g'),
     replace
   );
-}
+};
 
 module.exports = strReplaceAll;
--- test/test.js
+++ test/test.js
@@ -1,44 +1,46 @@
+'use strict';
+
 var      chai = require('chai'),
 strReplaceAll = require('../index');
 
 describe('str replace all', function () {
 
-  it("doesn't break on empty haystack", function () {
-    chai.expect(strReplaceAll("a", "b", "")).to.equal("");
+  it('does not break on empty haystack', function () {
+    chai.expect(strReplaceAll('a', 'b', '')).to.equal('');
   });
 
-  it("doesn't break on empty needle", function () {
-    chai.expect(strReplaceAll("", "d", "abc")).to.equal("abc");
+  it('does not break on empty needle', function () {
+    chai.expect(strReplaceAll('', 'd', 'abc')).to.equal('abc');
   });
 
-  it("doesn't break on empty replace", function () {
-    chai.expect(strReplaceAll("a", "", "abc")).to.equal("bc");
+  it('does not break on empty replace', function () {
+    chai.expect(strReplaceAll('a', '', 'abc')).to.equal('bc');
   });
 
-  it("works with a haystack that doesn't contain the needle", function () {
-    chai.expect(strReplaceAll("x", "y", "abc")).to.equal("abc");
+  it('works with a haystack that does not contain the needle', function () {
+    chai.expect(strReplaceAll('x', 'y', 'abc')).to.equal('abc');
   });
 
   it('replaces the needle(s) in a haystack that contains the needle once', function () {
-    chai.expect(strReplaceAll("a", "d", "abc")).to.equal("dbc");
+    chai.expect(strReplaceAll('a', 'd', 'abc')).to.equal('dbc');
   });
 
   it('replaces the needles in a haystack that contains the needle multiple times', function () {
-    chai.expect(strReplaceAll("a", "d", "abcabcabc")).to.equal("dbcdbcdbc");
+    chai.expect(strReplaceAll('a', 'd', 'abcabcabc')).to.equal('dbcdbcdbc');
   });
 
   it('replaces the needles in a haystack that contains only the needle multiple times', function () {
-    chai.expect(strReplaceAll("a", "d", "aaaaaa")).to.equal("dddddd");
+    chai.expect(strReplaceAll('a', 'd', 'aaaaaa')).to.equal('dddddd');
   });
 
   it('replaces the needle with a replace equal to needle', function () {
-    chai.expect(strReplaceAll("a", "a", "abc")).to.equal("abc");
+    chai.expect(strReplaceAll('a', 'a', 'abc')).to.equal('abc');
   });
 
   it('replaces a needle that contains special regexp characters', function () {
-    chai.expect(strReplaceAll("$", "x", "$bc")).to.equal("xbc");
-    chai.expect(strReplaceAll(".", "x", "ab.")).to.equal("abx");
-    chai.expect(strReplaceAll("^", "x", "a^c")).to.equal("axc");
+    chai.expect(strReplaceAll('$', 'x', '$bc')).to.equal('xbc');
+    chai.expect(strReplaceAll('.', 'x', 'ab.')).to.equal('abx');
+    chai.expect(strReplaceAll('^', 'x', 'a^c')).to.equal('axc');
   });
 
 });
