732c2cc05e90fa68a32da490a9745897c38885d3
strarsis <strarsis@gmail.com>
2015-09-15 00:04:29 +0200

    Add jsdoc annotations.

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 var escapeRegexp  = require('escape-string-regexp');
 
+/**
+ * Replaces all occurences of a string in another string.
+ * @constructor
+ * @param {string} needle   - The string to be searched.
+ * @param {string} replace  - The string which replaces the needle string.
+ * @param {string} haystack - The string which is searched for the needle string.
+ */
 var strReplaceAll = function(needle, replace, haystack) {
   if(needle.length === 0 || haystack.length === 0) {
     return haystack;
